83b94610339ae3dd0adc6623778553da13f3c2a7
php1301 <19520854@gm.uit.edu.vn>
2020-11-10 20:15:15 +0700

    Routing refactor

--- config/index.js
+++ config/index.js
@@ -1,9 +1,9 @@
 // const privateRoutes = require('./routes/privateRoutes');
-const publicRoutes = require('./routes/publicRoutes');
+// const publicRoutes = require('./routes/publicRoutes');
 
 const config = {
 //   privateRoutes,
-  publicRoutes,
+  // publicRoutes,
   port: process.env.PORT || '8080',
 };
 
