1ad0800ca3e747adb250bd22eff8dc867d32fa13
php1301 <19520854@gm.uit.edu.vn>
2020-11-15 23:13:43 +0700

    auth.service modified

--- services/auth.service.js
+++ services/auth.service.js
@@ -7,27 +7,28 @@ const authService = () => {
   const issue = (payload) => jwt.sign(payload, secret, { expiresIn: 10800 });
   const authenticate = (req, res, next) => {
     const token = req.header('token');
+    console.log(token);
     if (!token) {
       return res.status(401).json({
         message: 'You must provide token',
       });
     }
-    return jwtVerify(token, process.env.secretKey || 'mysecrettt')
+    return jwtVerify(token, secret)
       .then((decoded) => {
         if (decoded) {
-          console.log(decoded);
+          // console.log(decoded);
           req.user = decoded;
           return next();
         }
         return res.status(200).json({
           message: 'token is valid',
         });
-      });
+      }).catch((e) => console.log(e));
   };
   const authorize = (userTypeArray) => (req, res, next) => {
     console.log(req.user);
-    const { userType } = req.user;
-    if (userTypeArray.findIndex((elm) => elm === userType) !== -1) { return next(); }
+    const { role } = req.user.payload;
+    if (userTypeArray.findIndex((elm) => elm === role) !== -1) { return next(); }
     return res.status(401).json({ message: 'You dont have permission' });
   };
   return {
