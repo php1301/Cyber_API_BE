ded319893df2ee05caf28271ad2d2cbb22c28c0b
php1301 <19520854@gm.uit.edu.vn>
2020-11-15 23:13:23 +0700

    seat-type route added

--- api/routes.js
+++ api/routes.js
@@ -3,5 +3,6 @@ const express = require('express');
 const router = express.Router();
 
 router.use('/users', require('./user/index'));
+router.use('/seat-type', require('./seattype/index'));
 
 module.exports = router;
