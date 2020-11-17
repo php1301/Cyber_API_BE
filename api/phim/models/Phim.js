d835f72529c7248cd5c3861c28a72a99acc46cf4
php1301 <19520854@gm.uit.edu.vn>
2020-11-17 19:22:07 +0700

    Phim added

---
+++ api/phim/models/Phim.js
@@ -0,0 +1,68 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../../config/database');
+
+module.exports = () => {
+  const modelName = 'phim';
+  class Phim extends Sequelize.Model {}
+
+  // const Phim = db.define('Phim', {
+  Phim.init({
+    maPhim: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+    },
+    tenPhim: {
+      type: Sequelize.STRING,
+    },
+    trailer: {
+      type: Sequelize.STRING,
+    },
+    hinhAnh: {
+      type: Sequelize.STRING,
+    },
+    moTa: {
+      type: Sequelize.STRING,
+    },
+    maNhom: {
+      type: Sequelize.STRING,
+    },
+    ngayKhoiChieu: {
+      type: Sequelize.STRING,
+    },
+    danhGia: {
+      type: Sequelize.STRING,
+    },
+    daXoa: {
+      type: Sequelize.BOOLEAN,
+      default: false,
+    },
+    biDanh: {
+      type: Sequelize.STRING,
+    },
+  }, {
+    sequelize,
+    timestamps: true,
+    modelName,
+  });
+  /**
+     * -------------- ASSOCIATION ----------------
+     */
+  Phim.associate = function (models) {
+    Phim.belongsToMany(models.nhom, {
+      through: 'Phim_Nhom',
+    });
+    Phim.hasOne(models.lichchieu);
+  };
+  /**
+     * -------------- CLASS METHOD ----------------
+     */
+  // eslint-disable-next-line
+    Phim.toJSON = function () {
+    const values = Object.assign({}, this.get());
+
+    delete values.password;
+
+    return values;
+  };
+  return Phim;
+};
