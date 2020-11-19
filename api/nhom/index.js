2a848f832df12812209c051dbcad48e099335b59
php1301 <19520854@gm.uit.edu.vn>
2020-11-19 22:46:08 +0700

    create-nhom added

---
+++ api/nhom/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const controller = require('./nhom.controller');
+const authService = require('../../services/auth.service');
+
+const auth = authService();
+const nhomController = controller();
+const router = express.Router();
+router.post('/create-nhom', auth.authenticate, auth.authorize(['admin']), nhomController.taoNhom);
+
+module.exports = router;
