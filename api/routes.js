c000b52a71625c62f0b999c69f1291c14d03f91a
php1301 <19520854@gm.uit.edu.vn>
2020-11-19 22:45:20 +0700

    phim and nhom added

--- api/routes.js
+++ api/routes.js
@@ -4,5 +4,7 @@ const router = express.Router();
 
 router.use('/users', require('./user/index'));
 router.use('/seat-type', require('./seattype/index'));
+router.use('/phim', require('./phim/index'));
+router.use('/nhom', require('./nhom/index'));
 
 module.exports = router;
