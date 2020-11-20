9cb0315300330e35b32484f9211f02f880e9815c
php1301 <19520854@gm.uit.edu.vn>
2020-11-20 23:12:17 +0700

    user-list, user-pagination, user-info, delete-user added

--- api/phim/index.js
+++ api/phim/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const controller = require('./phim.controller');
+
+const phimController = controller();
 const authService = require('../../services/auth.service');
 
 const auth = authService();
-const phimController = controller();
 const router = express.Router();
 router.get('/', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layDanhSachPhim);
 router.post('/create-phim', auth.authenticate, auth.authorize(['admin']), phimController.themPhim);
--- api/user/index.js
+++ api/user/index.js
@@ -3,10 +3,17 @@ const express = require('express');
 const controller = require('./user.controller');
 
 const userController = controller();
+
+const authService = require('../../services/auth.service');
+
+const auth = authService();
+
 const router = express.Router();
 router.post('/register', userController.register);
 router.post('/login', userController.login);
-// router.post('/', authenticate, authorize(['client', 'admin']), stationController.postStation);
-
+router.get('/admin/user-list', auth.authenticate, auth.authorize(['admin']), userController.layDanhSachNguoiDung);
+router.get('/admin/user-list-pagination', auth.authenticate, auth.authorize(['admin']), userController.layDanhSachNguoiDungPhanTrang);
+router.get('/user-info', auth.authenticate, auth.authorize(['admin', 'client']), userController.layThongTinTaiKhoan);
+router.delete('/admin/delete-user', auth.authenticate, auth.authorize(['admin']), userController.xoaNguoiDung);
 
 module.exports = router;
