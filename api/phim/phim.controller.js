e46954854b326d5d6d72c8f0ef50946b7b0e2bdc
php1301 <19520854@gm.uit.edu.vn>
2020-11-22 21:48:50 +0700

    Phim Controllers logic added

--- api/phim/phim.controller.js
+++ api/phim/phim.controller.js
@@ -1,3 +1,4 @@
+const Sequelize = require('sequelize');
 const model = require('./models/Phim');
 
 const Phim = model();
@@ -8,8 +9,67 @@ const PhimController = () => {
       const phimData = await Phim.findAll({
         where: {
           maNhom,
-          tenPhim,
+          [Sequelize.Op.or]: {
+            tenPhim: {
+              [Sequelize.Op.contains]: tenPhim,
+            },
+          },
         },
+      });
+      res.status(200).json({ phimData });
+    } catch (e) {
+      console.log(e);
+      throw Error(e);
+    }
+  };
+  const layDanhSachPhimPhanTrang = async (req, res) => {
+    const {
+      page, pageSize, tenPhim, maNhom,
+    } = req.query;
+    const pageNum = parseInt(page || 0, 10);
+    const pageSizeNum = parseInt(pageSize || 10, 10);
+    const offset = pageNum * pageSizeNum;
+    const limit = pageSizeNum; try {
+      const phimData = await Phim.findAll({
+        where: {
+          maNhom,
+          [Sequelize.Op.or]: {
+            tenPhim: {
+              [Sequelize.Op.contains]: tenPhim,
+            },
+          },
+        },
+        offset,
+        limit,
+      });
+      res.status(200).json({ phimData });
+    } catch (e) {
+      console.log(e);
+      throw Error(e);
+    }
+  };
+  const layDanhSachPhimTheoNgay = async (req, res) => {
+    const {
+      page, pageSize, tenPhim, maNhom, startDate, endDate,
+    } = req.query;
+    const pageNum = parseInt(page || 0, 10);
+    const pageSizeNum = parseInt(pageSize || 10, 10);
+    const offset = pageNum * pageSizeNum;
+    const limit = pageSizeNum; try {
+      const phimData = await Phim.findAll({
+        where: {
+          maNhom,
+          [Sequelize.Op.or]: {
+            tenPhim: {
+              [Sequelize.Op.contains]: tenPhim,
+            },
+            ngayKhoiChieu: {
+              [Sequelize.Op.between]: [startDate, endDate],
+            },
+          },
+        },
+        offset,
+        limit,
       });
       res.status(200).json({ phimData });
     } catch (e) {
@@ -41,9 +101,31 @@ const PhimController = () => {
     }
     res.status(400).json({ msg: 'themPhim not succesfully' });
   };
+  const layThongTinPhim = async (req, res) => {
+    const { maPhim } = req.query;
+    const thongTinPhim = await Phim.findByPk(maPhim);
+    if (!thongTinPhim) res.status(400).json({ msg: 'Phim not exists' });
+    res.status(200).json({ thongTinPhim });
+  };
+  const xoaPhim = async (req, res) => {
+    const { maPhim } = req.body;
+    const affected = await Phim.destroy({
+      where: {
+        maPhim,
+      },
+    });
+    if (affected === 0) {
+      res.status(400).json({ msg: 'No Phim with this id found' });
+    }
+    res.status(200).json({ msg: 'Phim deleted' });
+  };
   return {
     layDanhSachPhim,
     themPhim,
+    layThongTinPhim,
+    xoaPhim,
+    layDanhSachPhimPhanTrang,
+    layDanhSachPhimTheoNgay,
   };
 };
 module.exports = PhimController;
