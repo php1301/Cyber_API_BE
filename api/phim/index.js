c15179bffd272b13e81d36116e7e0fa2192d3e8e
php1301 <19520854@gm.uit.edu.vn>
2020-11-22 21:45:45 +0700

    Phim Controllers add more

--- api/phim/index.js
+++ api/phim/index.js
@@ -6,8 +6,11 @@ const authService = require('../../services/auth.service');
 
 const auth = authService();
 const router = express.Router();
-router.get('/', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layDanhSachPhim);
+router.get('/phim', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layDanhSachPhim);
+router.get('/phim-paginate', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layDanhSachPhimPhanTrang);
+router.get('/phim-theo-ngay', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layDanhSachPhimTheoNgay);
+router.get('/lay-thong-tin-phim', auth.authenticate, auth.authorize(['admin', 'client']), phimController.layThongTinPhim);
 router.post('/create-phim', auth.authenticate, auth.authorize(['admin']), phimController.themPhim);
-
+router.delete('/delete-phim', auth.authenticate, auth.authorize(['admin']), phimController.xoaPhim);
 
 module.exports = router;
