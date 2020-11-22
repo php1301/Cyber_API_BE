e17f39810c4397f4b3102432079a6264fa6c980c
php1301 <19520854@gm.uit.edu.vn>
2020-11-22 21:55:24 +0700

    user-update route added

--- api/user/index.js
+++ api/user/index.js
@@ -14,6 +14,7 @@ router.post('/login', userController.login);
 router.get('/admin/user-list', auth.authenticate, auth.authorize(['admin']), userController.layDanhSachNguoiDung);
 router.get('/admin/user-list-pagination', auth.authenticate, auth.authorize(['admin']), userController.layDanhSachNguoiDungPhanTrang);
 router.get('/user-info', auth.authenticate, auth.authorize(['admin', 'client']), userController.layThongTinTaiKhoan);
+router.put('/user-update', auth.authenticate, auth.authorize(['admin', 'client']), userController.capNhatThongTinNguoiDung);
 router.delete('/admin/delete-user', auth.authenticate, auth.authorize(['admin']), userController.xoaNguoiDung);
 
 module.exports = router;
