784e69a58a175772ec7b47b92c5d0f107c6ad98e
php1301 <19520854@gm.uit.edu.vn>
2020-11-25 22:10:27 +0700

    Ticket model added

---
+++ api/ticket/models/Ticket.js
@@ -0,0 +1,85 @@
+const Sequelize = require('sequelize');
+const sequelize = require('../../../config/database');
+
+module.exports = () => {
+  const modelName = 'ticket';
+  class Ticket extends Sequelize.Model {}
+
+  Ticket.init({
+    maVe: {
+      type: Sequelize.STRING,
+      primaryKey: true,
+      unique: {
+        msg: 'Ticket id must be unique',
+      },
+    },
+    ngayDat: {
+      type: Sequelize.DATE,
+      allowNull: false,
+    },
+    giaVe: {
+      type: Sequelize.STRING,
+    },
+    taiKhoanNguoiDung: {
+      type: Sequelize.INTEGER,
+    },
+    maLichChieu: {
+      type: Sequelize.STRING,
+    },
+    soDT: {
+      type: Sequelize.STRING,
+    },
+    loaiGhe: {
+      type: Sequelize.STRING,
+    },
+    chietKhau: {
+      type: Sequelize.FLOAT,
+    },
+  }, {
+    sequelize,
+    createdAt: 'ngayDat',
+    updatedAt: 'updatedAt',
+    modelName,
+  });
+  /**
+     * -------------- ASSOCIATION ----------------
+     */
+  Ticket.associate = function (models) {
+    Ticket.hasMany(models.seat, { onDelete: 'CASCADE' });
+    Ticket.belongsTo(models.user, {
+      foreignKey: 'taiKhoanNguoiDung',
+      as: 'taiKhoanNguoiDung_ticket',
+    });
+    Ticket.belongsTo(models.lichchieu, {
+      foreignKey: 'maLichChieu',
+      as: 'maLichChieu_ticket',
+    });
+    /**
+           * -------------- SCOPES ----------------
+           */
+    // Ticket.addScope('datVe', {
+    //   include: [
+    //     {
+    //       model: models.usertype,
+    //       as: 'role',
+    //       required: true,
+    //       attributes: {
+    //         exclude: ['id', 'description'],
+    //       },
+    //     },
+    //   ],
+    // });
+  };
+  /**
+     * -------------- CLASS METHOD ----------------
+     */
+  // eslint-disable-next-line
+    Ticket.toJSON = function () {
+    const values = Object.assign({}, this.get());
+
+    delete values.password;
+
+    return values;
+  };
+  return Ticket;
+};
