b373528a2d846fd3f554ffdd9d5895326719af15
php1301 <19520854@gm.uit.edu.vn>
2020-11-27 23:18:08 +0700

    cinemar route added

--- api/routes.js
+++ api/routes.js
@@ -7,5 +7,6 @@ router.use('/seat-type', require('./seattype/index'));
 router.use('/phim', require('./phim/index'));
 router.use('/nhom', require('./nhom/index'));
 router.use('/ticket', require('./ticket/index'));
+router.use('/cinema', require('./cinema/index'));
 
 module.exports = router;
