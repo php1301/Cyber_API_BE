dab88c54a3e8f543033f6078f29fcbd267f00f72
php1301 <19520854@gm.uit.edu.vn>
2020-11-30 17:58:10 +0700

    datVe controller added with set association

--- api/ticket/ticket.controller.js
+++ api/ticket/ticket.controller.js
@@ -1,4 +1,4 @@
-const db = require('../../services/db.service');
+const models = require('../../services/db.service');
 
 // const model = require('./models/Ticket');
 // const seatModel = require('../seat/models/Seat');
@@ -7,27 +7,63 @@ const db = require('../../services/db.service');
 // const Seat = seatModel();
 const TicketController = () => {
   const datVe = async (req, res) => {
-    const sequelize = db().generateModel(false);
+    const db = models().generateModel(false);
     const { maLichChieu, danhSachVe, taiKhoanNguoiDung } = req.body;
-    try {
-      const result = await sequelize.sequelize.transaction(async (t) => {
-        const ticket = await sequelize.ticket.create({
-          maLichChieu,
-          taiKhoanNguoiDung,
-          seat_ticket: [
-            danhSachVe,
-          ],
-        }, {
-          include: [{ model: sequelize.seat, as: 'seat_ticket' }],
-          transaction: t,
-        });
-        return ticket;
-      }); // Managed transaction, auto rollback if error
-      return res.status(200).json({ result });
-    } catch (e) {
-      console.log(e);
-      return res.status(400).json({ msg: e.message });
+    if (maLichChieu && danhSachVe.length > 0 && taiKhoanNguoiDung) {
+      try {
+        const result = await db.sequelize.transaction(async (t) => {
+        // Kiểm tra ghế có thuộc lich chiêu ko
+        // Update ghế thành đã đạt
+          const gheCuaLichChieu = await db.lichchieu.scope('gheCuaLichChieu').findAll({
+            plain: true,
+            where: {
+              maLichChieu,
+            },
+          }, {
+            transaction: t,
+          });
+          // eslint-disable-next-line array-callback-return
+          const seatsToUpdate = [];
+          danhSachVe.map((i) => {
+            const index = gheCuaLichChieu
+              .cacLichChieuRap
+              .cacChoNgoiTrongRap
+              .findIndex((v) => v.maGhe === i.maGhe);
+            if (index === -1) {
+              throw Error('Ghế không thuộc rạp này');
+            }
+            if (
+              gheCuaLichChieu
+                .cacLichChieuRap
+                .cacChoNgoiTrongRap
+                .findIndex((v) => v.maGhe === i.maGhe && v.kichHoat === false) !== -1
+            // !==-1 vì index 0 là falsy
+            ) {
+              return seatsToUpdate.push(i.maGhe);
+            }
+          });
+          await db.seat.updateTrangThai(seatsToUpdate, t);
+          console.log(danhSachVe);
+          const ticket = await db.ticket.create({
+            maLichChieu,
+            taiKhoanNguoiDung,
+          }, {
+            transaction: t,
+          });
+          danhSachVe.map(async (i) => {
+            // Truyền mỗi PK thôi cũng được
+            await ticket.addDanhSachGhe(i.maGhe);
+          });
+          // WONTDO: handle giá vé, mã giảm giá, chiết khấu, giá vé có thể nằm trong body luôn
+          return ticket;
+        }); // Managed transaction, auto rollback if error
+        return res.status(200).json({ result });
+      } catch (e) {
+        console.log(e);
+        return res.status(400).json({ msg: e.message });
+      }
     }
+    return res.status(404).json({ msg: 'Vui lòng điền đầy đủ thông tin' });
   };
 
   return {
