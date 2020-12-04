9f74c096238ef1482f29a2c4dbd7409fed3d54ec
php1301 <19520854@gm.uit.edu.vn>
2020-12-04 22:51:56 +0700

    context added

--- services/auth.service.js
+++ services/auth.service.js
@@ -1,23 +1,27 @@
 const jwt = require('jsonwebtoken');
 const { jwtVerify } = require('../helpers/callback.helper');
-
+// Config của jwt
 const secret = process.env.NODE_ENV === 'production' ? process.env.JWT_SECRET : 'secret';
 
 const authService = () => {
+  // Tạo token, với payload muốn lưu trong token
   const issue = (payload) => jwt.sign(payload, secret, { expiresIn: '7d' });
   const authenticate = (req, res, next) => {
     const token = req.header('token');
     // console.log(token);
+    // Nếu không có token thì throw message error
     if (!token) {
       return res.status(401).json({
         message: 'You must provide token',
       });
     }
+    // decode và gửi payload này cho middleware tiếp theo
     return jwtVerify(token, secret)
       .then((decoded) => {
         if (decoded) {
           // console.log(decoded);
           req.user = decoded;
+          // next chạy middleware tiếp theo
           return next();
         }
         return res.status(200).json({
